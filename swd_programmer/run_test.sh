5bf039c025cef1c0b45f1e9767f154b5d138b19d
Caleb Parikh <parikhcaleb@gmail.com>
2025-11-23 11:24:30 +0000

    Remove swd_programmer

--- swd_programmer/run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Simple wrapper to run Python scripts with the virtual environment
-
-# Activate virtual environment
-source "$(dirname "$0")/venv/bin/activate"
-
-# Run the test script
-python3 test_swd.py "$@"
